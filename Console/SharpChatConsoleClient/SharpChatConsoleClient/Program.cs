f1ec39d429fed8162c7c3e68256be07ab00d028b
Govind Sahai <gsiitbhu@gmail.com>
2015-02-25 06:18:55 +0530

    Exception Handling added
    Next : Giving Some power to server!

--- Console/SharpChatConsoleClient/SharpChatConsoleClient/Program.cs
+++ Console/SharpChatConsoleClient/SharpChatConsoleClient/Program.cs
@@ -10,44 +10,120 @@ namespace SharpChatConsoleClient
     class Program
     {
         static NetworkStream networkStream = null;
+
+        enum STATUS
+        {
+            CONNECTED,
+            DISCONNECTED
+        }
+
+        static STATUS status { get; set; }
         static void Main(string[] args)
         {
+            TcpClient clientSocket = new TcpClient();
+            
+
             Console.Write("Enter IP to Connect to Server : ");
             String serverIPString = "10.8.101.4";// Console.ReadLine();
 
             Console.Write("\nEnter Port to Connect to Server : ");
             String serverPortString = "6969";//Console.ReadLine();
-            
-            TcpClient clientSocket = new TcpClient();
-            clientSocket.Connect(serverIPString, Int32.Parse(serverPortString));
-            Console.WriteLine("\nConnected to Server!");
-            networkStream = clientSocket.GetStream();
+
+            try
+            {
+                clientSocket.Connect(serverIPString, Int32.Parse(serverPortString));
+                status = STATUS.CONNECTED;
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("\nError Occurred in Connecting to Server!\n" +
+                    "Try Changing IP Address or Port or both.\n" +
+                    "Press Any Key to exit...\n");
+                Console.ReadKey();
+                status = STATUS.DISCONNECTED;
+                Environment.Exit(0);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nEither IP or Port or both are not in correct Format." +
+                    "Press Any Key to exit...\n");
+                status = STATUS.DISCONNECTED;
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            try
+            {
+                Console.WriteLine("\nConnected to Server!");
+                networkStream = clientSocket.GetStream();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Seems like Server Has Been closed!"+
+                    "Press Any Key to exit...\n");
+                Console.ReadKey();
+                status = STATUS.DISCONNECTED;
+                Environment.Exit(0);
+            }
 
             String MachineName = Environment.MachineName;
+            try
+            {
+                byte[] MNStream = Encoding.ASCII.GetBytes(MachineName + "$");
+                networkStream.Write(MNStream, 0, MNStream.Length);
 
-            byte[] MNStream = Encoding.ASCII.GetBytes(MachineName + "$");
-            networkStream.Write(MNStream, 0, MNStream.Length);
-
-            Thread t1 = new Thread(ServerChat);
-            t1.Start();
-
-            Thread t2 = new Thread(ClientChat);
-            t2.Start();
+                new Thread(ServerChat).Start();
+                new Thread(ClientChat).Start();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Error Occurred While Making Chat Room For " + MachineName +
+                    "\nPress Any Key to exit...\n");
+                Console.ReadKey();
+                status = STATUS.DISCONNECTED;
+                Environment.Exit(0);
+            }
         }
 
         static void ClientChat()
         {
             while (true)
             {
-                string outString = Console.ReadLine();
+                if (status == STATUS.DISCONNECTED)
+                {
+                    Console.WriteLine("Connection has been closed forcibly\n" +
+                    "\nPress Any Key to exit...\n");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+                Byte[] outStream = null;
+                try
+                {
+                    String outString = Console.ReadLine();
+                    if (outString.Length == 0)
+                    {
+                        Console.SetCursorPosition(0, Console.CursorTop - 1);
+                        continue;
+                    }
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    Console.WriteLine(" > Me : " + outString);
 
-                Console.SetCursorPosition(0, Console.CursorTop - 1);
-                Console.WriteLine(" > Me : "+outString);
-
-                outString = " > " + Environment.MachineName + " : " + outString;
-                byte[] outStream = Encoding.ASCII.GetBytes(outString + "$");
-
-                networkStream.Write(outStream, 0, outStream.Length);
+                    outString = " > " + Environment.MachineName + " : " + outString;
+                    outStream = Encoding.ASCII.GetBytes(outString + "$");
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Error while processing data\n" +
+                        "If you ofter seeing this Message, Please Restart");
+                    continue;
+                }
+                try
+                {
+                    networkStream.Write(outStream, 0, outStream.Length);
+                }
+                catch (Exception)
+                {
+                    status = STATUS.DISCONNECTED;
+                }
             }
         }
 
@@ -55,14 +131,27 @@ namespace SharpChatConsoleClient
         {
             while (true)
             {
+                if (status == STATUS.DISCONNECTED) 
+                {
+                    Console.WriteLine("Connection has been closed forcibly\n" +
+                    "\nPress Any Key to exit...\n");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
                 byte[] inStream = new byte[10025];
                 string inString = null;
 
-                networkStream.Read(inStream, 0, inStream.Length);
+                try
+                {
+                    networkStream.Read(inStream, 0, inStream.Length);
 
-                inString = Encoding.ASCII.GetString(inStream);
-                inString = inString.Substring(0, inString.IndexOf('$'));
-
+                    inString = Encoding.ASCII.GetString(inStream);
+                    inString = inString.Substring(0, inString.IndexOf('$'));
+                }
+                catch (Exception)
+                {
+                    status = STATUS.DISCONNECTED;
+                }
                 Console.WriteLine(inString);
             }
         }
--- Console/SharpChatConsoleServer/SharpChatConsoleServer/Program.cs
+++ Console/SharpChatConsoleServer/SharpChatConsoleServer/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace SharpChatConsoleServer
 {
@@ -23,67 +21,152 @@ namespace SharpChatConsoleServer
 
             Console.Write("\nEnter Port to Create Server : ");
             String serverPortString = "6969";// Console.ReadLine();
-            
-            IPAddress serverIP = IPAddress.Parse(serverIPString);
-            serverSocket = new TcpListener(serverIP, Int32.Parse(serverPortString));
-            serverSocket.Start();
+
+            try
+            {
+                IPAddress serverIP = IPAddress.Parse(serverIPString);
+                serverSocket = new TcpListener(serverIP, Int32.Parse(serverPortString));
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("\nEither IP or Port or both are not in correct Format."+
+                    "Press Any Key to exit...\n");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            try
+            {
+                serverSocket.Start();
+            }
+            catch(SocketException)
+            {
+                Console.WriteLine("\nError Occurred in Creating Server!\n"+
+                    "Try Changing IP Address or Port or both.\n"+
+                    "Press Any Key to exit...\n");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
 
             Console.WriteLine("\nServer Started at IP : {0}, Port : {1}", serverIPString, serverPortString);
-
-            new Thread(getClientConnection).Start();
-            new Thread(ServerChat).Start();
+            try
+            {
+                new Thread(getClientConnection).Start();
+                new Thread(ServerChat).Start();
+            }
+            catch (ThreadStateException)
+            {
+                Console.WriteLine("\nThe Thread Has already been started. Cannot Start Again.");
+            }
+            catch(OutOfMemoryException)
+            {
+                Console.WriteLine("\nOutOfMemoryException Thrown.\n"+
+                    "There is not enough memory available to start this Process.\n"+
+                    "Press Any Key To exit...");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
         }
 
         static void getClientConnection()
         {
             while (true)
             {
-                TcpClient clientSocket = serverSocket.AcceptTcpClient();
-                handleClient newClient = new handleClient(clientSocket);
-                clientsList.Add(newClient);
+                try
+                {
+                    TcpClient clientSocket = serverSocket.AcceptTcpClient();
+                    handleClient newClient = new handleClient(clientSocket);
+                    clientsList.Add(newClient);
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("\nUnknown Error Occurred\n"+
+                        "If you ofter seeing this Message, Please Restart Server");
+                }
+            }
+        }
+
+        public static void deleteClientConnection(handleClient toDeleteClient)
+        {
+            try
+            {
+                clientsList.Remove(toDeleteClient);
+            }
+            catch(Exception)
+            {
+                
             }
         }
 
         static void ServerChat()
         {
             while (true)
-            {   
-                string outString = Console.ReadLine();
-
-                Console.SetCursorPosition(0, Console.CursorTop - 1);
-                Console.WriteLine(" > Me : " + outString);
-
-                byte[] outStream = Encoding.ASCII.GetBytes(" > Server : " + outString + "$");
-
-                foreach (var client in clientsList)
+            {
+                try
                 {
-                    client.FromServerChat(outStream);
+                    String outString = Console.ReadLine();
+                    if (outString.Length == 0)
+                    {
+                        Console.SetCursorPosition(0, Console.CursorTop - 1);
+                        continue;
+                    }
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    Console.WriteLine(" > Me : " + outString);
+
+                    byte[] outStream = Encoding.ASCII.GetBytes(" > Server : " + outString + "$");
+
+                    foreach (var client in clientsList)
+                    {
+                        client.FromServerChat(outStream);
+                    }
+                }
+                catch(IOException)
+                {
+                    Console.WriteLine("\nError Reading/Writing to Console.");
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("\nUnknown Error Occurred\n" +
+                        "If you ofter seeing this Message, Please Restart Server");
                 }
             }
         }
 
         public static void broadcastInputStream(byte[] broadcastStream,handleClient fromClient)
         {
-            NetworkStream networkStream = null;
-            foreach (var otherClients in clientsList)
+            try
             {
-                if (otherClients == fromClient)
-                    continue;
-                networkStream = otherClients.clientSocket.GetStream();
-                networkStream.Write(broadcastStream, 0, broadcastStream.Length);
+                NetworkStream networkStream = null;
+                foreach (var otherClients in clientsList)
+                {
+                    if (otherClients == fromClient)
+                        continue;
+                    networkStream = otherClients.clientSocket.GetStream();
+                    networkStream.Write(broadcastStream, 0, broadcastStream.Length);
+                }
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("\nError Occured While BroadCasting the Message of " + fromClient.MachineName);
             }
         }
 
         public static void broadcastInputString(String broadcastString, handleClient fromClient)
         {
-            byte[] broadcastStream = Encoding.ASCII.GetBytes(broadcastString);
-            NetworkStream networkStream = null;
-            foreach (var otherClients in clientsList)
+            try
             {
-                if (otherClients == fromClient)
-                    continue;
-                networkStream = otherClients.clientSocket.GetStream();
-                networkStream.Write(broadcastStream, 0, broadcastStream.Length);
+                byte[] broadcastStream = Encoding.ASCII.GetBytes(broadcastString);
+                NetworkStream networkStream = null;
+                foreach (var otherClients in clientsList)
+                {
+                    if (otherClients == fromClient)
+                        continue;
+                    networkStream = otherClients.clientSocket.GetStream();
+                    networkStream.Write(broadcastStream, 0, broadcastStream.Length);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nError Occured While BroadCasting the Message of " + fromClient.MachineName);
             }
         }
     }
@@ -92,46 +175,103 @@ namespace SharpChatConsoleServer
     {
         public TcpClient clientSocket { get; set; }
         public String MachineName { get; set; }
+
+        private enum STATUS
+        {
+            CONNECTED,
+            DISCONNECTED
+        }
+        private STATUS status { get; set; }
         public handleClient(TcpClient clientSocketTemp)
         {
-            this.clientSocket = clientSocketTemp;
+            try
+            {
+                this.clientSocket = clientSocketTemp;
 
-            NetworkStream networkStream = clientSocket.GetStream();
-            
-            byte[] clientNameByte = new byte[1000];
-            networkStream.Read(clientNameByte, 0, clientNameByte.Length);
-            this.MachineName = Encoding.ASCII.GetString(clientNameByte);
-            this.MachineName = this.MachineName.Substring(0, this.MachineName.IndexOf('$'));
-            Console.WriteLine("--- " + this.MachineName + " Connected ---");
+                NetworkStream networkStream = clientSocket.GetStream();
 
-            Server.broadcastInputString("--- " + this.MachineName + " Connected ---$", this);
-
-            new Thread(FromClientChat).Start();
+                byte[] clientNameByte = new byte[1000];
+                networkStream.Read(clientNameByte, 0, clientNameByte.Length);
+                this.MachineName = Encoding.ASCII.GetString(clientNameByte);
+                this.MachineName = this.MachineName.Substring(0, this.MachineName.IndexOf('$'));
+                this.status = STATUS.CONNECTED;
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("--- " + this.MachineName + " Tried to Connect ---");
+                Server.broadcastInputString("--- " + this.MachineName + " Tried to Connect ---$", this);
+                        
+                this.status = STATUS.DISCONNECTED;
+            }
+            try
+            {
+                if (status == STATUS.CONNECTED)
+                {
+                    Console.WriteLine("--- " + this.MachineName + " Connected ---");
+                    Server.broadcastInputString("--- " + this.MachineName + " Connected ---$", this);
+                }
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("\nUnknown Error Occured But You Can continue.");
+            }
+            try
+            {
+                new Thread(FromClientChat).Start();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("--- " + this.MachineName + " DISConnected ---");
+                Server.broadcastInputString("--- " + this.MachineName + " DISConnected ---$", this);
+                this.status = STATUS.DISCONNECTED;
+            }
+            if (status == STATUS.DISCONNECTED)
+                Server.deleteClientConnection(this);
         }
 
         private void FromClientChat()
         {
             while (true)
             {
+                if (status == STATUS.DISCONNECTED) 
+                {
+                    Server.deleteClientConnection(this);
+                    break;
+                }
                 byte[] inStream = new byte[10025];
                 string inString = null;
+                try
+                {
+                    NetworkStream networkStream = this.clientSocket.GetStream();
+                    networkStream.Read(inStream, 0, inStream.Length);
 
-                NetworkStream networkStream = this.clientSocket.GetStream();
-                networkStream.Read(inStream, 0, inStream.Length);
-
-                inString = Encoding.ASCII.GetString(inStream);
-                inString = inString.Substring(0, inString.IndexOf('$'));
-
-                Console.WriteLine(inString);
-
-                Server.broadcastInputStream(inStream, this);
+                    inString = Encoding.ASCII.GetString(inStream);
+                    inString = inString.Substring(0, inString.IndexOf('$'));
+                    Console.WriteLine(inString);
+                    Server.broadcastInputStream(inStream, this);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("--- " + this.MachineName + " DISConnected ---");
+                    Server.broadcastInputString("--- " + this.MachineName + " DISConnected ---$", this);
+                    this.status = STATUS.DISCONNECTED;
+                }
             }
         }
 
         public void FromServerChat(byte[] outStream)
         {
-            NetworkStream networkStream = this.clientSocket.GetStream();
-            networkStream.Write(outStream, 0, outStream.Length);
+            if (status == STATUS.DISCONNECTED)
+                Server.deleteClientConnection(this);
+            try
+            {
+                NetworkStream networkStream = this.clientSocket.GetStream();
+                networkStream.Write(outStream, 0, outStream.Length);
+            }
+            catch(Exception)
+            {
+                //  Unknown Error
+            }
         }
     }
 }
